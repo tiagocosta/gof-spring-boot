21aeed5ae61d423f124d3a75aaa64771c0ad8845
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-03 12:51:13 -0300

    Initial commit

---
+++ src/main/java/com/tcs/gof_spring_boot/GofSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.gof_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/gof_spring_boot/GofSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.gof_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
